c428b66d2bfa2dfee862c41abd8df1655a43f29a
Julien Beral <julien.beraldevaulx@gmail.com>
2022-11-16 17:27:41 +0100

    first page 90%

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,39 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const user = document.createElement('div')
+        user.classList.add('user');
+        const link = document.createElement('a');
+        link.href = "index.html";
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute("alt", name)
+        const h2 = document.createElement('h2');
         h2.textContent = name;
-        article.appendChild(img);
+        const p = document.createElement('p');
+        p.textContent = city + ", " + country;
+        const p2 = document.createElement('p');
+        p2.textContent = tagline;
+        const p3 = document.createElement('p');
+        p3.textContent = price + "€/jour";
+
+        article.appendChild(user);
+        article.appendChild(link);
         article.appendChild(h2);
+        article.appendChild(p);
+        article.appendChild(p2);
+        article.appendChild(p3);
+        user.appendChild(img);
+        user.appendChild(h2);
+        link.appendChild(user);
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+
+    return { name, city, country, tagline, price, picture, getUserCardDOM };
 }
+
+
