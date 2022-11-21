d398364070534a61c4b75cfc7b857721f6b4451b
Julien Beral <julien.beraldevaulx@gmail.com>
2022-11-21 17:57:11 +0100

    filter + headband + media list done

--- scripts/factories/portfolio.js
+++ scripts/factories/portfolio.js
@@ -1,6 +1,6 @@
 function photographerHeader(data) {
 
-    const { name, city, country, tagline, portrait } = data;
+    const { name, city, country, tagline, portrait, price, likes } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -38,5 +38,102 @@ function photographerHeader(data) {
 
     }
 
-    return { name, city, country, tagline, picture, getPhotographerHeaderDOM };
+    return { name, city, country, tagline, picture, price, likes, getPhotographerHeaderDOM };
 }
+
+function mediaFactory(data) {
+
+    const { image, video, title, likes, date, id, photographerId } = data;
+
+    function getMediaDOM() {
+
+        const mediaCard = document.createElement('article');
+        mediaCard.classList.add('media-card');
+
+        if (data.image) {
+
+            const img = document.createElement('img');
+
+            img.setAttribute("tabindex", "4");
+            img.setAttribute("src", `assets/media/${photographerId}/${image}`);
+            img.setAttribute("alt", title);
+            img.setAttribute("role", "img");
+            img.setAttribute("aria-label", title);
+
+            mediaCard.appendChild(img);
+        } else if (data.video) {
+
+            const video = document.createElement('video');
+            const mp4 = `assets/media/${photographerId}/${data.video}`;
+            const source = document.createElement("source");
+
+            video.setAttribute("tabindex", "4");
+            video.setAttribute("controls", " ");
+            source.setAttribute("src", mp4);
+            source.setAttribute("alt", title);
+            source.setAttribute("type", "video/mp4");
+
+            video.appendChild(source);
+            mediaCard.appendChild(video);
+        }
+
+        const mediaText = document.createElement('div');
+        mediaText.classList.add('media-text');
+        const buttonLike = document.createElement('div');
+        buttonLike.classList.add('button-like');
+        const h2 = document.createElement('h2');
+        h2.textContent = title;
+        const h2_likes = document.createElement('h2');
+        h2_likes.textContent = likes;
+        const heartImg = document.createElement('img');
+        heartImg.setAttribute("src", "assets/icons/heart.svg");
+        heartImg.setAttribute("alt", "likes");
+        heartImg.setAttribute("role", "img");
+        heartImg.setAttribute("aria-label", "likes");
+
+        mediaText.appendChild(h2);
+        mediaText.appendChild(h2_likes);
+        mediaText.appendChild(heartImg);
+        buttonLike.appendChild(h2_likes);
+        buttonLike.appendChild(heartImg);
+        mediaText.appendChild(buttonLike);
+
+
+        mediaCard.appendChild(mediaText);
+
+        return (mediaCard);
+
+    }
+
+    return { image, video, title, likes, date, id, photographerId, getMediaDOM };
+}
+
+function headband() {
+
+    const band = document.querySelector(".headband");
+    const like = document.createElement("h2")
+    const heart = document.createElement("img");
+    const price = document.createElement("h2");
+    const likeClass = document.createElement('div');
+    const priceClass = document.createElement('div');
+    likeClass.classList.add('likeClass');
+    priceClass.classList.add('priceClass');
+
+    like.id = "total_likes";
+    heart.setAttribute("src", "assets/icons/black-heart.svg");
+    price.id = "price";
+
+
+    band.appendChild(likeClass);
+    band.appendChild(priceClass);
+    band.appendChild(like);
+    band.appendChild(heart);
+    band.appendChild(price);
+    likeClass.appendChild(like);
+    likeClass.appendChild(heart);
+    priceClass.appendChild(price);
+
+    return band;
+}
+
+headband();
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,6 +1,7 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const photographId = urlParams.get('id');
+const menuChoice = document.querySelector(".choice");
 
 async function getPhotographers() {
     const response = await fetch("data/photographers.json");
@@ -12,6 +13,7 @@ async function getPhotographers() {
 function displayData(photographers) {
 
     const photographerSections = document.querySelector(".photograph-header");
+    let headband_price = 0;
 
     photographers.forEach(photographer => {
         if (photographer.id == photographId) {
@@ -19,13 +21,74 @@ function displayData(photographers) {
             const userCardDOM = photographerModel.getPhotographerHeaderDOM();
             photographerSections.appendChild(userCardDOM);
 
+            headband_price += photographerModel.price;
+            console.log(headband_price);
+
         }
     });
+
+    const headband_prices = document.getElementById("price");
+    headband_prices.textContent = headband_price + "€ / jour";
+
+}
+
+function displayMedia(media) {
+
+    const mediaSections = document.querySelector(".photograph-medias-container");
+
+    media.forEach(media => {
+        if (media.photographerId == photographId) {
+
+            const mediaModel = mediaFactory(media);
+            const mediaDOM = mediaModel.getMediaDOM();
+            mediaSections.appendChild(mediaDOM);
+
+        }
+    });
+
+    let totalLikes = 0;
+    media.forEach(media => {
+        if (media.photographerId == photographId) {
+            const mediaModel = mediaFactory(media);
+            const userMediaDOM = mediaModel.getMediaDOM();
+            mediaSections.appendChild(userMediaDOM);
+            totalLikes += mediaModel.likes;
+            console.log(totalLikes);
+        }
+    })
+
+    let total_likes = document.getElementById("total_likes");
+    total_likes.textContent = totalLikes;
+}
+
+function filterMedia(media) {
+
+    switch (menuChoice.value) {
+
+        case "popular":
+            media.sort((a, b) => b.likes - a.likes);
+            break;
+
+        case "date":
+            media.sort((a, b) => new Date(b.date) - new Date(a.date));
+            break;
+
+        case "title":
+            media.sort((a, b) => a.title.localeCompare(b.title));
+            break;
+    }
+
+    const mediaSections = document.querySelector(".photograph-medias-container");
+    mediaSections.textContent = "";
+
+    displayMedia(media);
 
 }
 
 async function display() {
-    const { photographers } = await getPhotographers();
+    const { photographers, media } = await getPhotographers();
     displayData(photographers);
+    displayMedia(media);
+    menuChoice.addEventListener("change", () => filterMedia(media));
 }
 display(); 
